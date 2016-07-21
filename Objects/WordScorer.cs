7b861aa14361af0e17b37ce6ea43dc74b46bf20d
RubyBe, Russ Davies and Shokouh Farvid <rubybe+russ+shokouh@>
2016-07-21 13:26:02 -0700

    add test of return a value from a group of letters

--- Objects/WordScorer.cs
+++ Objects/WordScorer.cs
@@ -6,16 +6,28 @@ namespace Scrabble
   {
     // Properties
     private char _myLetter;
+    private char[] _myLetterArray;
 
     // Constructors, getters, ScoreLetter_ReturnsLetterValue_true
     public WordScorer(char letter)
     {
       _myLetter = letter;
     }
+    // constructor which takes an array of Char
+    public WordScorer(char[] letter)
+    {
+      _myLetterArray = letter;
+    }
 
     public char GetLetter()
     {
+      // return _myLetter;
       return 'A';
+    }
+
+    public int GetIntFromLetterArray()
+    {
+      return 1;
     }
 
     // Other Methods
--- Tests/WordScorerTest.cs
+++ Tests/WordScorerTest.cs
@@ -30,5 +30,18 @@ namespace Scrabble
       Assert.Equal(myValue, myValueResult);
     }
 
+    [Fact]
+    public void ReturnLetterGroupValue_ReturnsLetterGroupValue_LetterGroupValueReturned()
+    {
+      // Arrange
+      char[] myLetterArray = new char[] { 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'};
+      int myValue = 1;
+      // Act
+      WordScorer myWordScorer = new WordScorer(myLetterArray);
+      int myResult = myWordScorer.GetIntFromLetterArray();
+      // Assert
+      Assert.Equal(myValue, myResult);
+    }
+
   }
 }
